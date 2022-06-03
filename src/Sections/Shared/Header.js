c4c247dd092e80f08ee4388ddb8850e1fb49b7aa
Shama <assiddik001@gmail.com>
2022-06-03 14:19:24 +0600

    header done

--- src/Sections/Shared/Header.js
+++ src/Sections/Shared/Header.js
@@ -3,81 +3,109 @@ import { Link } from "react-router-dom";
 import search from "../../assets/img/search.png";
 import heart from "../../assets/img/heart.png";
 import shoppingBag from "../../assets/img/u_shopping-bag.png";
-import user from "../../assets/img/user.png";
 import menuRight from "../../assets/img/menu-right.png";
 import chevronDown from "../../assets/img/chevron-down.png";
 import uPercentage from "../../assets/img/u_percentage.png";
 import Logo from "../../Components/Logo";
 
 const Header = () => {
+  const mainMenu = (
+    <>
+      <li>
+        <Link to="/">Home </Link>
+      </li>
+      <li>
+        <Link to="/">Shop </Link>
+      </li>
+      <li>
+        <Link to="/">Pages </Link>
+      </li>
+      <li>
+        <Link to="/">Orders </Link>
+      </li>
+      <li>
+        <Link to="/">Blogs </Link>
+      </li>
+      <li>
+        <Link to="/">Contact </Link>
+      </li>
+      <li>
+        <Link to="/">Track Order </Link>
+      </li>
+    </>
+  );
   return (
     <div>
-      <div class="navbar bg-base-100 flex justify-between p-0">
-        <div class="">
+      <div className="navbar bg-base-100 flex justify-between lg:p-0">
+        <div className="">
           <Logo />
         </div>
         <div className="hidden lg:block">
-          <div class="border border-primary rounded-full flex items-center">
-            <div class="min-w-max px-3">
+          <div className="border border-primary rounded-full flex items-center">
+            <div className="min-w-max px-3">
               <img src={search} alt="" />
             </div>
             <input
               type="text"
               placeholder="Search"
-              class="input  focus:outline-none "
+              className="input  focus:outline-none "
             />
             <button
-              class="bg-neutral bs-button-bg px-6 rounded-full py-2 mr-1 text-base-100"
+              className="bg-neutral bs-button-bg px-6 rounded-full py-2 mr-1 text-base-100"
               type="submit"
             >
               Search
             </button>
           </div>
         </div>
-        <div class="flex-none">
-          <div class="dropdown dropdown-end">
-            <label tabindex="0" class="btn btn-ghost btn-circle">
-              <div class="indicator">
+        <div className="flex-none">
+          <div className="dropdown dropdown-end">
+            <label tabIndex="0" className="btn btn-ghost btn-circle">
+              <div className="indicator">
                 <img src={heart} alt="" />
-                <span class="badge badge-sm indicator-item">8</span>
+                <span className="badge badge-sm indicator-item">8</span>
               </div>
             </label>
             <div
-              tabindex="0"
-              class="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow"
+              tabIndex="0"
+              className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow"
             >
-              <div class="card-body">
-                <span class="font-bold text-lg">8 Items</span>
-                <span class="text-info">Subtotal: $999</span>
-                <div class="card-actions">
-                  <button class="btn btn-primary btn-block">View cart</button>
+              <div className="card-body">
+                <span className="font-bold text-lg">8 Items</span>
+                <span className="text-info">Subtotal: $999</span>
+                <div className="card-actions">
+                  <button className="btn btn-primary btn-block">
+                    View cart
+                  </button>
                 </div>
               </div>
             </div>
           </div>
-          <div class="dropdown dropdown-end">
-            <label tabindex="0" class="btn btn-ghost btn-circle">
-              <div class="indicator">
+          <div className="dropdown dropdown-end">
+            <label tabIndex="0" className="btn btn-ghost btn-circle">
+              <div className="indicator">
                 <img src={shoppingBag} alt="" />
-                <span class="badge badge-sm indicator-item">8</span>
+                <span className="badge badge-sm indicator-item">8</span>
               </div>
             </label>
             <div
-              tabindex="0"
-              class="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow"
+              tabIndex="0"
+              className="mt-3 card card-compact dropdown-content w-52 bg-base-100 shadow"
             >
-              <div class="card-body">
-                <span class="font-bold text-lg">8 Items</span>
-                <span class="text-info">Subtotal: $999</span>
-                <div class="card-actions">
-                  <button class="btn btn-primary btn-block">View cart</button>
+              <div className="card-body">
+                <span className="font-bold text-lg">8 Items</span>
+                <span className="text-info">Subtotal: $999</span>
+                <div className="card-actions">
+                  <button className="btn btn-primary btn-block">
+                    View cart
+                  </button>
                 </div>
               </div>
             </div>
           </div>
-          <div class="dropdown dropdown-end">
-            <label tabindex="0" class="btn btn-ghost btn-circle avatar">
-              <div class="w-10 rounded-full">
+          <div className="dropdown dropdown-end">
+            <label tabIndex="0" className="btn btn-ghost btn-circle avatar">
+              <div className="w-10 rounded-full">
                 <img
                   alt=""
                   src="https://api.lorem.space/image/face?hash=33791"
@@ -85,13 +113,13 @@ const Header = () => {
               </div>
             </label>
             <ul
-              tabindex="0"
-              class="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
+              tabIndex="0"
+              className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-base-100 rounded-box w-52"
             >
               <li>
-                <a href="/" class="justify-between">
+                <a href="/" className="justify-between">
                   Profile
-                  <span class="badge">New</span>
+                  <span className="badge">New</span>
                 </a>
               </li>
               <li>
@@ -101,6 +129,58 @@ const Header = () => {
                 <a href="/">Logout</a>
               </li>
             </ul>
+          </div>
+        </div>
+      </div>
+      <div>
+        <div className="navbar bg-base-100">
+          <div className="navbar-start">
+            <div className="dropdown">
+              <label
+                tabIndex="0"
+                className="btn btn-ghost bg-primary rounded-full hover:bg-neutral"
+              >
+                <img src={menuRight} alt="" />
+                <span className="mx-4 text-base-100 hidden lg:block">
+                  All Catagories
+                </span>
+                <img src={chevronDown} alt="" className=" hidden lg:block" />
+              </label>
+              <ul
+                tabIndex="0"
+                className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-accent rounded-box w-52"
+              >
+                <li>
+                  <Link to="/">Homepage</Link>
+                </li>
+                <li>
+                  <Link to="/">Portfolio</Link>
+                </li>
+                <li>
+                  <Link to="/">About</Link>
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="navbar-center">
+            <div className="dropdown">
+              <label tabIndex="0" className="btn btn-ghost lg:hidden">
+                Main Menu
+              </label>
+              <ul
+                tabIndex="0"
+                className="menu menu-compact dropdown-content mt-3 p-2 shadow bg-accent rounded-box w-52"
+              >
+                {mainMenu}
+              </ul>
+            </div>
+            <div className="navbar-center hidden lg:flex">
+              <ul className="menu menu-horizontal p-0">{mainMenu}</ul>
+            </div>
+          </div>
+          <div className="navbar-end text-secondary">
+            <img src={uPercentage} alt="" />
+            <span className="text-sm">Special Offers</span>
           </div>
         </div>
       </div>
