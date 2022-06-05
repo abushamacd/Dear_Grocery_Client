ac13aaff5c64c712bfb9ee45aae3691dcd2a7dd3
Shama <assiddik001@gmail.com>
2022-06-05 16:23:39 +0600

    git hub login implement

--- src/Sections/User/SocialLogin.js
+++ src/Sections/User/SocialLogin.js
@@ -3,6 +3,7 @@ import { FaGoogle, FaGithub, FaFacebookSquare } from "react-icons/fa";
 import auth from "../../firebase.init";
 import {
   useSignInWithFacebook,
+  useSignInWithGithub,
   useSignInWithGoogle,
 } from "react-firebase-hooks/auth";
 import Loading from "../Shared/Loading";
@@ -15,29 +16,33 @@ const SocialLogin = () => {
   // Facebook
   const [signInWithFacebook, fbUser, fbLoading, fbError] =
     useSignInWithFacebook(auth);
+  // Github
+  const [signInWithGithub, githubUser, githubLoading, githubError] =
+    useSignInWithGithub(auth);
+
   // navigate
   const navigate = useNavigate();
 
   // Loading
-  if (googleLoading || fbLoading) {
+  if (googleLoading || fbLoading || githubLoading) {
     return <Loading />;
   }
   // Error
   let errorMessage;
-  if (googleError || fbError) {
+  if (googleError || fbError || githubError) {
     errorMessage = (
       <p className="text-red-600">
         {" "}
-        {googleError?.message} {fbError?.message}{" "}
+        {googleError?.message} {fbError?.message} {githubError?.message}{" "}
       </p>
     );
   }
 
   // user
-  if (googleUser || fbUser) {
+  if (googleUser || fbUser || githubUser) {
     navigate("/home");
   }
-  console.log(googleUser, fbUser);
+  console.log(googleUser, fbUser, githubUser);
   return (
     <div>
       <div className="flex justify-evenly">
@@ -48,7 +53,7 @@ const SocialLogin = () => {
           <FaGoogle />
         </button>
         <button
-          //   onClick={() => signInWithGoogle()}
+          onClick={() => signInWithGithub()}
           className="btn btn-outline btn-primary w-12 rounded-full"
         >
           <FaGithub />
